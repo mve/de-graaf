287d533413ff77bf1a4e5832eb2dd66cc36cd3d7
Hidde <hidhulshof@gmail.com>
2019-11-15 21:31:24 +0100

    Reserveringen beter opgehaald

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -9,7 +9,10 @@ class ReservationController extends Controller
 {
     public function adminGet()
     {
-        $reservations = Reservation::all();
+        $unsortedreservations = Reservation::where('people', '>', 0);
+        $sorted = $unsortedreservations->orderBy('date','asc');
+        $reservations = $sorted->paginate(6);
+
         return view('admin.reservations', compact('reservations'));
     }
 }
