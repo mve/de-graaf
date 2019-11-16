0761b3807743864ad95a2267a6ed27551180d6b1
Hidde <hidhulshof@gmail.com>
2019-11-16 18:12:18 +0100

    fixedfouten

--- app/Table.php
+++ app/Table.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Table extends Model
 {
     public function reservation(){
-        return $this->belongsToMany('App/Reservation', 'reservation_tables', 'reservation_id', 'table_id');
+        return $this->belongsToMany('App/Reservation', 'reservation_tables', 'table_id', 'reservation_id');
 
     }
 }
