bfe9a408f58ccf7b30b8e5dcb233a9fe945c7cb9
Mike van Egmond <m.vanegmond@freave.com>
2019-11-19 13:13:34 +0100

    Added logo, favicon, custom hamburger menu button.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,14 +1,14 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+            <img class="navbar-brand-image" src="{{url('images/logo-transparent-no-bottom-text.png')}}">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
+            <img style="max-width: 27px;" src="{{url('images/hamburger-menu.png')}}">
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class=" collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
