5e56c0ace41c25b79019656d23201d81f8e98277
Mike van Egmond <m.vanegmond@freave.com>
2019-11-25 13:12:48 +0100

    Added menu page

--- database/seeds/ReservationTableSeeder.php
+++ database/seeds/ReservationTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ReservationTableSeeder extends Seeder
 {
@@ -11,21 +12,21 @@ class ReservationTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\MainCourse::class, 4)->create();
-        factory(App\SubCourse::class, 8)->create();
-        factory(App\Product::class, 8)->create();
 
+//        factory(App\MainCourse::class, 5)->create();
+//        factory(App\SubCourse::class, 8)->create();
+//        factory(App\Product::class, 8)->create();
 
         factory(App\Table::class, 8)->create();
+
         factory(App\User::class, 20)->create()->each(function ($u) {
             factory(App\Reservation::class, 3)->create(['user_id' => $u->id]);
-
         });
+
         factory(App\Reservation_table::class, 60)->create();
 
         factory(App\Receipt::class, 60)->create()->each(function ($r) {
             factory(App\Order::class, 3)->create(['receipt_id' => $r->id]);
-
         });
 
     }
